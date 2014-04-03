6dac6590ddfbeb1ce4e805895874550f116914a6
Nikolai V. Santos <santoskola@gmail.com>
2014-04-03 16:41:25 -0500

    products deletion

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Project::Application.routes.draw do
   
-  get    'products',           to: 'products#index', as: 'products'
-  get    'products/new' ,      to: 'products#new',   as: 'new_product'
-  get    'products/:id' ,      to: 'products#show',  as: 'product'
-  post   'products' ,          to: 'products#create'
-  get    'products/:id/edit' , to: 'products#edit' , as: 'edit_product'
-  patch  'products/:id' ,      to: 'products#update'
-
+  get    'products',            to: 'products#index', as: 'products'
+  get    'products/new' ,       to: 'products#new',   as: 'new_product'
+  get    'products/:id' ,       to: 'products#show',  as: 'product'
+  post   'products' ,           to: 'products#create'
+  get    'products/:id/edit' ,  to: 'products#edit' , as: 'edit_product'
+  patch  'products/:id' ,       to: 'products#update'
+  delete 'products/:id'   ,     to: 'products#destroy' , as: 'delete_product'
   
   root 'products#index'
  
