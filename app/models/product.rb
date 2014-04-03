ca5639aa992a56ae06beb5a5c8a6796355998092
Nikolai V. Santos <santoskola@gmail.com>
2014-04-03 14:21:35 -0500

    added some validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+  validates :title , :price , :stock_quantity , presence: true
+  validates :price , numericality: {greater_thatn_or_equal_to: 0.01}
+  validates :stock_quantity,  numericality: {only_integer: true}
 end
