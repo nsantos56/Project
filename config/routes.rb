7bdef060963a4c1d27fffee6975ba13dd2e1739d
Nikolai V. Santos <santoskola@gmail.com>
2014-04-03 14:05:57 -0500

    new and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Project::Application.routes.draw do
-  get 'products',      to: 'products#index', as: 'products'
-  get 'products/:id' , to: 'products#show',  as: 'product'
-  get 'products/new' , to: 'products#new',  as: 'new_product'
+  
+  get  'products',      to: 'products#index', as: 'products'
+  get  'products/new' , to: 'products#new',  as: 'new_product'
+  get  'products/:id' , to: 'products#show',  as: 'product'
+  post 'products' ,     to: 'products#create'
+  
   
   root 'products#index'
  
