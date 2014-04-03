997810bb5e52bf1786f09ab03648f97e665533c9
Nikolai V. Santos <santoskola@gmail.com>
2014-04-03 13:07:57 -0500

    added a root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Project::Application.routes.draw do
   get 'products', to: 'products#index', as: 'products'
+  
+  root 'products#index'
  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
